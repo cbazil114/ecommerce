634626bca1ea98bf4b6600c4b3f4129bb67607b4
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-01-04 16:28:11 -0500

    Finished product and category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,12 +5,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  Product.findAll({ include: [Category, Tag] })
+    .then(products => res.json(products))
+    .catch(err => res.status(500).json(err.message))
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
+  Product.findOne({ where: { id: req.params.id }, include: [Tag, Category] })
+    .then(products => res.json(products))
+    .catch(err => res.status(500).json(err.message))
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -91,6 +97,9 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({ where: { id: req.params.id } })
+    .then(products => res.json(products))
+    .catch(err => res.status(500).json(err.message))
 });
 
 module.exports = router;
